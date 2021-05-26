a36585a8ddc7ac449f8ce1b1460d92d0e160f749
jaehwan <dkdlrk5208@gmail.com>
2021-05-26 11:06:24 +0900

    add Bulma CSS

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div>
     <section class="hero is-primary">
       <div class="hero-body">
-        <p class="title">강아지와 고양이의 동행</p>
-        <p class="subtitle">강아지와 고양이에 대한 모든 정보를 볼 수 있는 곳</p>
+        <p class="title">영화 소개</p>
+        <p class="subtitle">해외/국내 평론사이트별 평점 순위 (모든 영화)</p>
       </div>
 
     </section>
