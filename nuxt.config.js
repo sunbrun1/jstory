90884b3f49bfc43d81f9b7c6a10f0e02198237bc
jaehwan <dkdlrk5208@gmail.com>
2021-05-26 10:41:52 +0900

    add Bulma CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,15 +3,21 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'jstory',
+    title: '영화 소개',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'hi' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "stylesheet",  
+        href: "https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css"
+      }
+    ],
+    script: [{defer: "",
+              src:"https://use.fontawesome.com/releases/v5.15.3/js/all.js"
+            }]
   },
   /*
   ** Customize the progress bar color
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "upgrade"
+git commit -m "add Bulma CSS"
 git push -u origin main
