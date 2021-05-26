434e3c6e4dd21fab8a5d284cae69b44f743b7789
jaehwan <dkdlrk5208@gmail.com>
2021-05-26 11:40:28 +0900

    add movieimage

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,92 @@
         <p class="title">영화 소개</p>
         <p class="subtitle">해외/국내 평론사이트별 평점 순위 (모든 영화)</p>
       </div>
-
     </section>
+    <div class="columns">
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>1. 분노의 질주: 더 얼티메이트</p>
+          </div>
+          <div class="message-body">
+            <figure class="image is-2by3">
+              <img src="@/assets/분노의질주.jpg" alt="" width="200px" height="200px">
+            </figure>
+            기다림은 끝났다! 
+            전 세계가 기다려온 단 하나의 액션블록버스터!  
+
+            도미닉(빈 디젤)은 자신과 가장 가까웠던 형제 제이콥(존 시나)이 사이퍼(샤를리즈 테론)와 연합해
+            전 세계를 위기로 빠트릴 위험천만한 계획을 세운다는 사실을 알게 되고,
+            이를 막기 위해 다시 한 번 패밀리들을 소환한다.
+            
+            가장 가까운 자가 한순간, 가장 위험한 적이 된 상황
+            도미닉과 패밀리들은 이에 반격할 놀라운 컴백과 작전을 세우고
+            지상도, 상공도, 국경도 경계가 없는 불가능한 대결이 시작되는데…
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>2. 도라에몽: 스탠바이미 2</p>
+          </div>
+          <div class="message-body">
+            <figure class="image is-2by3">
+              <img src="@/assets/도라에몽.jpg" alt="">
+            </figure>
+            “할머니를 만나고 싶어!”
+            어느 날 진구는 돌아가신 할머니와의 추억이 담긴 낡은 곰 인형을 발견하고
+            할머니가 보고 싶은 마음에 도라에몽의 4차원 비밀도구 타임머신을 타고 과거로 향한다.
+            느닷없이 찾아온 소년을 금세 진구라고 믿어주시는 할머니는 한 가지 소원을 말씀하시는데…
+
+            “진구의 아내를 만나보고 싶구나”
+            결혼식을 보여 드리기 위해 미래로 간 도라에몽과 진구!
+            하지만 이슬이와의 결혼식 당일 신랑 진구는 도망가 버리고
+            퉁퉁이와 비실이가 진구를 찾는 동안 이슬이는 진구를 믿고 기다리는데…
+            할머니의 소원을 위해, 그리고 가족과 친구들, 좋아하는 이슬이와의 약속을 위해
+            도라에몽과 진구의 과거와 현재, 미래를 넘나드는 스펙터클한 대모험이 시작된다!
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>극장판 귀멸의 칼날: 무한열차편</p>
+          </div>
+          <div class="message-body">
+              <figure class="image is-2by3">
+                <img src="@/static/귀멸의칼날.jpg" alt="">
+              </figure>
+            혈귀로 변해버린 여동생 ‘네즈코’를 인간으로 되돌릴 단서를 찾아 비밀조직 귀살대에 들어간 ‘탄지로.’ 
+            ‘젠이츠’, ‘이노스케’와 새로운 임무 수행을 위해 무한열차에 탑승 후 귀살대 최강 검사 염주 ‘렌고쿠’와 합류한다. 
+
+            달리는 무한열차에서 승객들이 하나 둘 흔적 없이 사라지자 숨어있는 식인 혈귀의 존재를 직감하는 ‘렌고쿠’. 
+
+            귀살대 ‘탄지로’ 일행과 최강 검사 염주 ‘렌고쿠’는 어둠 속을 달리는 무한열차에서 모두의 목숨을 구하기 위해 예측불가능한 능력을 가진 혈귀와 목숨을 건 혈전을 시작하는데…
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            
+          </div>
+          <div class="message-body">
+            
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            
+          </div>
+          <div class="message-body">
+            
+          </div>
+        </article>
+      </div>
+    </div>
   </div>
 </template>
 
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "add fontawesome icons "
+git commit -m "add movieimage "
 git push -u origin main
