1e6b189fcc540940b7b91034576b6b72513fae9b
jaehwan <dkdlrk5208@gmail.com>
2021-06-09 10:37:46 +0900

    fill the table defined in https://yts.mx/api#list_movies

--- pages/movies.vue
+++ pages/movies.vue
@@ -2,19 +2,49 @@
     <div>
         <section class="hero is-warning">
             <div class="hero-body">
-                <p class="title"> 강아지 품종 </p>
-                <p class="subtitle"> 다양한 강아지 품종을 함께 공부합시다. </p>
-
+                <p class="title"> 다운로드 별 영화 순위 </p>
             </div>
         </section>
         <hr>
         <section class="columns">
             <div class="column"></div>
+                <table class="table">
+                    <thead>
+                        <th>순위</th>
+                        <th>제목</th>
+                        <th>평점</th>
+                        <th>상영시간</th>
+
+                    </thead>
+                    <tbody>
+                        <template v-for="pos in moviesByDownload.length">
+                            <tr :key="pos">
+                                <td>{{pos}}</td>
+                                <td>{{moviesByDownload[pos-1].title}}</td>
+                                <td>{{moviesByDownload[pos-1].rating}}</td>
+                                <td>{{moviesByDownload[pos-1].runtime}} 분</td>
+                            </tr>
+                        </template>
+                    </tbody>
+                </table>
             <div class="column"></div>
         </section>
 
     </div>
 </template>
+
 <script>
-    
+    import axios from 'axios';
+
+    export default{
+        async asyncData(){
+            const movies = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=download_count');
+            console.log(Object.keys(movies))
+            console.log(movies.data.data.movies)
+        
+            return {
+                moviesByDownload : movies.data.data.movies
+            };
+        }
+    }
 </script>
--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m " add axios module "
+git commit -m " fill the table defined in https://yts.mx/api#list_movies "
 git push -u origin main
